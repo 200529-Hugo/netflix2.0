7ba2f110d98f77a0b5ff36498dc3e35795a98285
Hugo de Heus <200529@student.glu.nl>
2022-03-25 14:16:40 +0100

    Trailer test

--- src/components/movie.js
+++ src/components/movie.js
@@ -1,12 +1,19 @@
 import React from "react";
 
+const KEY_API = "4a3df646f07f4da444876af8342211e9"
 const IMG_API = "https://image.tmdb.org/t/p/w1280";
+const TRAILER_API = "http://api.themoviedb.org/3/movie/"
 
-const Movie = ({title, poster_path, overview, vote_average}) => (
+const Movie = ({id, title, poster_path, overview, vote_average}) => (
     <div className="movie">
+        <script>
+            TRAILER_API+id+"/videos?api_key="+ KEY_API
+        </script>
         <img src={IMG_API + poster_path} alt={title} />
         <div className="movie-info">
-            movie
+            {id}
+            {TRAILER_API+id+"/videos?api_key="+ KEY_API}
+            <iframe src={test} title=""></iframe>
         </div>
     </div>
 )
